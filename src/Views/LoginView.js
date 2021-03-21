4476c9c78b751c969f8f66eb429e1f2a3909ce72
Dmytro <tschernomord@gmail.com>
2021-03-21 15:53:58 +0100

    add yup and validation

--- src/Views/LoginView.js
+++ src/Views/LoginView.js
@@ -2,49 +2,86 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import s from './Form.module.css';
 import authOperations from '../redux/auth/authOperation';
+import { string } from 'yup';
 
+// const validateSchema = object().shape({
+//   email: string().required().email(),
+//   password: string().required()
+// })
 class LoginView extends Component {
   state = {
     email: '',
     password: '',
+    validateEmail: string().required().email(),
+    validatePassword: string().required(),
+    errors: {
+      email: '',
+      password: '',
+    },
+  };
+  capitalize = s => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+  handleOnChange = async e => {
+    const { name, value } = e.currentTarget;
+    await this.setState({ [name]: value });
+  };
+  handeValidateOnBlur = e => {
+    const { name } = e.currentTarget;
+    try {
+      this.state[`validate${this.capitalize(name)}`].validateSync(
+        this.state[name],
+      );
+      this.setState(state => ({ errors: { ...state.errors, [name]: '' } }));
+    } catch (error) {
+      if (error.errors[0]) {
+        this.setState(state => ({
+          errors: { ...state.errors, [name]: error.errors[0] },
+        }));
+      }
+    }
   };
 
-  handleOnChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-  };
   handleSubmit = e => {
     e.preventDefault();
     this.props.login({ ...this.state });
-
     this.setState({ email: '', password: '' });
   };
   render() {
     return (
       <form onSubmit={this.handleSubmit} className={s.form}>
         <label className={s.label}>
-          Email{' '}
+          Email
           <input
             className={s.input}
             onChange={this.handleOnChange}
+            onBlur={this.handeValidateOnBlur}
             name="email"
             value={this.state.email}
             type="email"
             placeholder="Email"
             required
           />
+          {this.state.errors.email && (
+            <dir className={s.error}>{this.state.errors.email}</dir>
+          )}
         </label>
         <label className={s.label}>
-          Password{' '}
+          Password
           <input
             className={s.input}
             onChange={this.handleOnChange}
+            onBlur={this.handeValidateOnBlur}
             name="password"
             value={this.state.password}
             type="password"
             placeholder="Password"
             required
           />
+          {this.state.errors.password && (
+            <dir className={s.error}>{this.state.errors.password}</dir>
+          )}
         </label>
         <button className={s.button} type="submit">
           Log in
--- src/Views/RegisterView.js
+++ src/Views/RegisterView.js
@@ -2,17 +2,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import s from './Form.module.css';
 import authOperations from '../redux/auth/authOperation';
+import { string } from 'yup';
 
 class RegisterView extends Component {
   state = {
-    name: '',
-    email: '',
-    password: '',
+    values: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    // name: '',
+    // email: '',
+    // password: '',
+    validateName: string().required(),
+    validateEmail: string().required().email(),
+    validatePassword: string().required(),
+    errors: {
+      name: '',
+      email: '',
+      password: '',
+    },
   };
-
+  capitalize = s => {
+    if (typeof s !== 'string') return '';
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
   handleOnChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
+    this.setState(state => ({ values: { ...state.values, [name]: value } }));
+
+    // this.setState({ [name]: value });
+  };
+  handeValidateOnBlur = e => {
+    const { name } = e.currentTarget;
+    try {
+      this.state[`validate${this.capitalize(name)}`].validateSync(
+        this.state.values[name],
+      );
+      this.setState(state => ({ errors: { ...state.errors, [name]: '' } }));
+    } catch (error) {
+      if (error.errors[0]) {
+        this.setState(state => ({
+          errors: { ...state.errors, [name]: error.errors[0] },
+        }));
+      }
+    }
   };
   handleSubmit = e => {
     e.preventDefault();
@@ -28,33 +62,48 @@ class RegisterView extends Component {
           <input
             className={s.input}
             onChange={this.handleOnChange}
+            onBlur={this.handeValidateOnBlur}
             name="name"
-            value={this.state.name}
+            value={this.state.values.name}
             type="text"
             placeholder="Name"
+            required
           />
+          {this.state.errors.name && (
+            <dir className={s.error}>{this.state.errors.name}</dir>
+          )}
         </label>
         <label className={s.label}>
           Email{' '}
           <input
             className={s.input}
             onChange={this.handleOnChange}
+            onBlur={this.handeValidateOnBlur}
             name="email"
-            value={this.state.email}
+            value={this.state.values.email}
             type="email"
             placeholder="Email"
+            required
           />
+          {this.state.errors.email && (
+            <dir className={s.error}>{this.state.errors.email}</dir>
+          )}
         </label>
         <label className={s.label}>
           Password{' '}
           <input
             className={s.input}
             onChange={this.handleOnChange}
+            onBlur={this.handeValidateOnBlur}
             name="password"
-            value={this.state.password}
+            value={this.state.values.password}
             type="password"
             placeholder="Password"
+            required
           />
+          {this.state.errors.password && (
+            <dir className={s.error}>{this.state.errors.password}</dir>
+          )}
         </label>
         <button className={s.button} type="submit">
           Sign up
@@ -63,12 +112,6 @@ class RegisterView extends Component {
     );
   }
 }
-// const mapStateToProps = (state) => ({
-
-// })
-
-// const mapDispatchToProps = {};
-
 export default connect(null, { onRegistration: authOperations.registration })(
   RegisterView,
 );
